7e964086fac1c3d937fd0814704693fedd4b230b
golchi <golchigeek@gmail.com>
2021-11-04 22:11:31 +0530

    making trending section api

--- controllers/trendingMovie.js
+++ controllers/trendingMovie.js
@@ -3,6 +3,7 @@ exports.trendingsection = async(req ,res ,next)=>{
     movieModel.find({} ,(err ,item)=>{
         if(err){
             console.log(err);
+            res.json(err);
         }
         else{
             console.log(item);
