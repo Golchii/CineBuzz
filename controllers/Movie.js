6263a5cab3eb2763900a62e0a59e55f6d1406a49
golchi <golchigeek@gmail.com>
2021-11-25 22:52:40 +0530

    error handle

--- controllers/Movie.js
+++ controllers/Movie.js
@@ -17,7 +17,7 @@ exports.trendingsection = async(req ,res ,next)=>{
             console.log(err);
         }
         else{
-            for(let i=0 ; i < 5 ; i++){
+            for(let i=0 ; i < 10 ; i++){
                 a.push(item[i]);
             }
             console.log(a);
@@ -97,14 +97,20 @@ exports.onemovie = async(req ,res , next)=>{
 }
 exports.onemovieRatingshow=async(req,res,next)=>{
     movieModel.find({_id:req.body.Movieid},(err,item)=>{
-        let sum = 0;
-        let l=0;
-        for(l = 0; l<size(item[0].ratingArr);l++){
-            sum= sum + item[0].ratingArr[l].rating
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
         }
-        res.statusCode = 201;
-        console.log((sum/l).toPrecision(2)+"");
-        res.json((sum/l).toPrecision(2)+"");
+        else{
+            let sum = 0;
+            let l=0;
+            for(l = 0; l<size(item[0].ratingArr);l++){
+                sum= sum + item[0].ratingArr[l].rating
+            }
+            res.statusCode = 201;
+            console.log((sum/l).toPrecision(2)+"");
+            res.json((sum/l).toPrecision(2)+"");
+        }
     })
 }
 exports.onemovieRating = async(req , res ,next)=>{
@@ -116,74 +122,98 @@ exports.onemovieRating = async(req , res ,next)=>{
         rating:rating
     }
     movieModel.findOne({_id:Movieid},(err,item)=>{
-        let x = true;
-        for(var k=0 ; k< size(item.ratingArr); k++){
-            if(userid===item.ratingArr[k].userid){
-                console.log("again rate");
-                item.ratingArr[k].rating = rating;
-                item.save();
-                x=false;
-            }
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
         }
-        if(x===true){
-            item.ratingArr.push(list);
-            item.save();
+        else{
+            let x = true;
+            for(var k=0 ; k< size(item.ratingArr); k++){
+                if(userid===item.ratingArr[k].userid){
+                    console.log("again rate");
+                    item.ratingArr[k].rating = rating;
+                    item.save();
+                    x=false;
+                }
+            }
+            if(x===true){
+                item.ratingArr.push(list);
+                item.save();
+            }
+            res.statusCode =201;
+            res.json('done');
         }
     })
-    res.statusCode =201;
-    res.json('done');
 }
 exports.yourRating = async(req,res,next)=>{
     const userid = req.body.userid;
     const Movieid = req.body.Movieid;
     movieModel.findOne({_id:Movieid},(err,item)=>{
-        for(var k = 0 ; k < size(item.ratingArr) ; k++){
-            if(userid===item.ratingArr[k].userid){
-                console.log('your rating');
-                res.statusCode = 201;
-                return res.json(item.ratingArr[k].rating+"");
-            }
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
         }
-        res.statusCode = 301;
-        return res.json("0")
+        else{
+            for(var k = 0 ; k < size(item.ratingArr) ; k++){
+                if(userid===item.ratingArr[k].userid){
+                    console.log('your rating');
+                    res.statusCode = 201;
+                    return res.json(item.ratingArr[k].rating+"");
+                }
+            }
+            res.statusCode = 301;
+            return res.json("0");
+        }
     })
 }
 exports.refreshArr = async(req,res,next)=>{
     user.findOne({_id:req.body.userid},(err,item)=>{
-        item.token = [];
-        item.save();
-        res.statusCode = 201;
-        res.json('done');
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            item.token = [];
+            item.save();
+            res.statusCode = 201;
+            res.json('done');
+        }
     })
 }
 exports.randomfxn = async(req , res , next)=>{
     const userid = req.body.userid;
     movieModel.find({genre:req.body.genre},(err ,result)=>{
-        s = size(result);
-        var x = Math.floor(Math.random()*(s));
-        user.findOne({_id:userid},(err,item)=>{
-            var i=0;
-            for(i = 0 ; i < size(item.token) ; i++){
-                console.log(item.token);
-                if(size(item.token)==s){ 
-                    console.log('khatam');
-                    item.token = [];
-                    item.save();
-                    res.statusCode=301;
-                    return res.json('empty');
-                }
-                if(x==item.token[i]){
-                    console.log('loop')
-                    x = Math.floor(Math.random()*(s));
-                    console.log(x);
-                    i=-1;
-                }
-            }  
-            item.token.push(x);
-            item.save();
-            console.log(x);
-            res.json(result[x]);
-        })
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            s = size(result);
+            var x = Math.floor(Math.random()*(s));
+            user.findOne({_id:userid},(err,item)=>{
+                var i=0;
+                for(i = 0 ; i < size(item.token) ; i++){
+                    console.log(item.token);
+                    if(size(item.token)==s){ 
+                        console.log('khatam');
+                        item.token = [];
+                        item.save();
+                        res.statusCode=301;
+                        return res.json('empty');
+                    }
+                    if(x==item.token[i]){
+                        console.log('loop')
+                        x = Math.floor(Math.random()*(s));
+                        console.log(x);
+                        i=-1;
+                    }
+                }  
+                item.token.push(x);
+                item.save();
+                console.log(x);
+                res.json(result[x]);
+            })
+        }
     });
 }
 exports.onemovieReview = async(req ,res ,next)=>{
@@ -195,101 +225,143 @@ exports.onemovieReview = async(req ,res ,next)=>{
         review:review
     }
     movieModel.findOne({_id:Movieid},(err,item)=>{
-        let x = true;
-        for(var k=0 ; k< size(item.reviewArr); k++){
-            if(userid===item.reviewArr[k].userid){
-                item.reviewArr[k].review = review;
-                res.json('review edited')
-                item.save();
-                x=false;
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            let x = true;
+            for(var k=0 ; k< size(item.reviewArr); k++){
+                if(userid===item.reviewArr[k].userid){
+                    item.reviewArr[k].review = review;
+                    res.json('review edited')
+                    item.save();
+                    x=false;
+                }
             }
+            if(x===true){
+                item.reviewArr.push(list);
+                res.json('review saved')
+                item.save();
+            }
+            res.statusCode = 201;
         }
-        if(x===true){
-            item.reviewArr.push(list);
-            res.json('review saved')
-            item.save();
-        }
-        res.statusCode = 201;
     })
 }
 exports.userdetails = async(req,res,next)=>{
     const userid =req.body.userid;
     user.findOne({_id:userid},'dpUrl name',(err,item)=>{
-        res.statusCode =202;
-        res.json(item)
-        console.log(item);
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            res.statusCode =202;
+            res.json(item)
+            console.log(item);
+        }
     })
 }
 exports.onemovieReviewshow=async(req,res,next)=>{
     const Movieid = req.body.Movieid;
     movieModel.findOne({_id:Movieid},async(err,item)=>{
-        res.statusCode = 201;
-        res.json(item.reviewArr);
-        console.log(item.reviewArr);
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            res.statusCode = 201;
+            res.json(item.reviewArr);
+            console.log(item.reviewArr);
+        }
     })
 }
 exports.history = async(req ,res ,next)=>{
     const userid = req.body.userid;
     const Movieid = req.body.Movieid;
     movieModel.findOne({_id:Movieid},(err,result)=>{
-        result.views++;
-        result.save();
-        user.findOne({_id:userid},(err,item)=>{
-            let x = true;
-            for(let k = 0 ; k < size(item.history) ; k++){
-                if(Movieid===item.history[k]){
-                    console.log('again');
-                    res.statusCode = 301;
-                    res.json('again');
-                    x = false;
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            result.views++;
+            result.save();
+            user.findOne({_id:userid},(err,item)=>{
+                let x = true;
+                for(let k = 0 ; k < size(item.history) ; k++){
+                    if(Movieid===item.history[k]){
+                        console.log('again');
+                        res.statusCode = 301;
+                        res.json('again');
+                        x = false;
+                    }
                 }
-            }
-            if(x===true){
-                item.history.push(Movieid);
-                res.statusCode = 201;
-                res.json('added in history');
-                item.save();
-            }
-        })
+                if(x===true){
+                    item.history.push(Movieid);
+                    res.statusCode = 201;
+                    res.json('added in history');
+                    item.save();
+                }
+            })
+        }
     })
 }
 exports.showHistory = async (req , res ,next)=>{
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        console.log(item.history);
-        res.statusCode = 201;
-        res.json(item.history);
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            console.log(item.history);
+            res.statusCode = 201;
+            res.json(item.history);
+        }
     })
 }
 exports.movieCount = async (req ,res ,next)=>{
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        console.log(size(item.history));
-        res.statusCode = 201;
-        res.json(size(item.history)+"");
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            console.log(size(item.history));
+            res.statusCode = 201;
+            res.json(size(item.history)+"");
+        }
     })
 }
 exports.onemovieWishlist = async(req,res,next)=>{
     const Movieid =req.body.Movieid;
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        let x = true;
-        console.log(size(item.wishlistArr));
-        for(let k =0 ; k<size(item.wishlistArr);k++){
-            if(Movieid===item.wishlistArr[k]){
-                console.log("again");
-                item.wishlistArr.splice(k,1);
-                res.statusCode = 301;
-                res.json('removed from wishlist')
-                x=false;
-                item.save()
-            }
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
         }
-        if(x===true){
-            item.wishlistArr.push(Movieid);
-            res.statusCode =201;
-            res.json('added to wishlist')
-            item.save();
+        else{
+            let x = true;
+            console.log(size(item.wishlistArr));
+            for(let k =0 ; k<size(item.wishlistArr);k++){
+                if(Movieid===item.wishlistArr[k]){
+                    console.log("again");
+                    item.wishlistArr.splice(k,1);
+                    res.statusCode = 301;
+                    res.json('removed from wishlist')
+                    x=false;
+                    item.save()
+                }
+            }
+            if(x===true){
+                item.wishlistArr.push(Movieid);
+                res.statusCode =201;
+                res.json('added to wishlist')
+                item.save();
+            }
         }
     })
 }
@@ -297,36 +369,54 @@ exports.onemovieWishlistshow = async(req,res,next)=>{
     const Movieid =req.body.Movieid;
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        let x = true;
-        for(let k =0 ; k<size(item.wishlistArr);k++){
-            if(Movieid===item.wishlistArr[k]){
-                console.log(1);
-                res.json("1");
-                x = false;
-            }
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
         }
-        if(x===true){
-            console.log(0);
-            res.json("0");
+        else{
+            let x = true;
+            for(let k =0 ; k<size(item.wishlistArr);k++){
+                if(Movieid===item.wishlistArr[k]){
+                    console.log(1);
+                    res.json("1");
+                    x = false;
+                }
+            }
+            if(x===true){
+                console.log(0);
+                res.json("0");
+            }
         }
     });
 }
 exports.Allwishlist = async (req ,res,next)=>{
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        console.log(item.wishlistArr);
-        res.statusCode = 201;
-        res.json(item.wishlistArr);
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            console.log(item.wishlistArr);
+            res.statusCode = 201;
+            res.json(item.wishlistArr);
+        }
     })
 }
 exports.deleteHistory = async(req , res ,next)=>{
     const userid = req.body.userid;
     user.findOne({_id:userid},(err,item)=>{
-        item.history=[];
-        item.save();
+        if(err){
+            console.log(err);
+            res.statusCode = 301;
+        }
+        else{
+            item.history=[];
+            item.save();
+            res.statusCode = 201;
+            res.json('history cleared')
+        }
     })
-    res.statusCode = 201;
-    res.json('history cleared')
 }
 exports.feedback = async(req ,res,next)=>{
     const email= req.body.email;
